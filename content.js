d5df698b0c7868de846b48af9c03072b6558e5a1
Nishir <nshelat@usc.edu>
2017-01-21 15:21:18 -0800

    Changed image to gray square

--- content.js
+++ content.js
@@ -2,11 +2,11 @@ var images = document.getElementsByTagName('img');
 var srcList = [];
 for(var i = 0; i < images.length; i++) {
 	if(document.getElementsByTagName("img")[i].getAttribute("id")) {
-		document.getElementById(document.getElementsByTagName("img")[i].getAttribute("id")).style.display = "none";
+		document.getElementById(document.getElementsByTagName("img")[i].getAttribute("id")).src = "http://docs.tfs-timetracker.com/download/attachments/2720425/TFS_Timetracker_Client_TrackingStatusGray.png?version=1&modificationDate=1460101655607&api=v2";
 	}
 	else {
 		document.getElementsByTagName("img")[i].setAttribute("id", "censored");
-		document.getElementById("censored").style.display = "none";
+		document.getElementById("censored").src = "http://docs.tfs-timetracker.com/download/attachments/2720425/TFS_Timetracker_Client_TrackingStatusGray.png?version=1&modificationDate=1460101655607&api=v2";
 	}
     srcList.push(images[i].src);
 }
