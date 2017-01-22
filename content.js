7f180a648efa4b6182b6fee449ef1ff78056b25a
Nishir <nshelat@usc.edu>
2017-01-21 16:34:12 -0800

    Added tilted Nikias photo

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ function start() {
 
 	for(var i = 0; i < images.length; i++) {
 		if(document.getElementsByTagName("img")[i].getAttribute("id")) {
-			document.getElementById(document.getElementsByTagName("img")[i].getAttribute("id")).src = "https://nishirshelat.com/images/nikias.jpg";
+			document.getElementById(document.getElementsByTagName("img")[i].getAttribute("id")).src = "https://about.usc.edu/files/2011/07/Nikias.jpg";
 			document.getElementById(document.getElementsByTagName("img")[i].getAttribute("id")).style = "object-fit: fill";
 		}
 		else {
@@ -20,7 +20,7 @@ function start() {
 	}
 
 	for(i = 0; i < numCustomClasses; i++) {
-		document.getElementsByClassName("censored")[i].src = "https://news.usc.edu/files/2013/01/Nikias.jpg";
+		document.getElementsByClassName("censored")[i].src = "https://about.usc.edu/files/2011/07/Nikias.jpg";
 		document.getElementsByClassName("censored")[i].style = "object-fit: fill";
 	}
 }
